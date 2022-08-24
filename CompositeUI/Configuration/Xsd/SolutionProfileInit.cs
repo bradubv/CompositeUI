0f51d26db8a71f7316e648019ded022de2376dfb
bogdan.bradu <bradubv@hotmail.com>
2022-08-24 02:16:29 -0500

    Added SolutionProfileInit to fix Step1 of Walk and ModuleInit to fix Step 2

---
+++ CompositeUI/Configuration/Xsd/SolutionProfileInit.cs
@@ -0,0 +1,44 @@
+﻿//===============================================================================
+// Microsoft patterns & practices
+// Smart Client Software Factory 2010
+//===============================================================================
+// Copyright © Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//===============================================================================
+//===============================================================================
+// Microsoft patterns & practices
+// CompositeUI Application Block
+//===============================================================================
+// Copyright © Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+// Copyright © cntsoftware.com
+
+namespace Cnt.CompositeUI.Configuration.Xsd
+{
+    /// <summary>
+    /// Partial class to initialize solution profile.
+    /// </summary>
+    public partial class SolutionProfileElement
+    {
+        /// <summary>
+        /// Constructor used to initialize empty moduleinfo.
+        /// </summary>
+        public SolutionProfileElement()
+        {
+            modulesField = new ModuleInfoElement[0];
+        }
+    }
+}
